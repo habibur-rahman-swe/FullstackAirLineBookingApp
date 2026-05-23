8289ac4686e5421c7bdeaeba8cb8de60670c5da2
Habibur Rahman <habibur0199mhr@gmail.com>
2026-05-23 22:00:16 +0600

    initial commit

---
+++ backend/HRAirline/src/main/java/com/hr/airline/HrAirlineApplication.java
@@ -0,0 +1,13 @@
+package com.hr.airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAirlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAirlineApplication.class, args);
+	}
+
+}
